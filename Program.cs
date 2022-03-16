cae934021be6f14e953d23db864b94bb5163e49d
veroni <veroni_vieira-jun1@estudante.sesisenai.org.br>
2022-03-15 23:19:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DEVinBank
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var cliente = new Cliente();
+            Console.WriteLine("Bem vindo a seu banco DEVinBank!\r\n\r\n\r\n---------------- Crie uma Conta Corrente, Poupança ou Investimento---------------\r\n\r\n\r\n------------------ Digite seus dados abaixo para criar sua conta ----------------\r\n\r\n\r\n");
+            Console.WriteLine("Digita seu nome: ");
+            
+            
+  
+       
+        }
+    }
+}
